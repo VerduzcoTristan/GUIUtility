35434b0bc24f561b5b17280688f323001211c9a5
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2021-02-10 10:45:33 +0000

    Updated UIManager.java
    Added taskDelay and taskPeriod to openGUI(Player, UserInterface), to enable customisation of the Update task without having to refactor code.
    
    Removed UserInterface reference on getItem(UIEntry).
    
    Added Exceptions to setupInventory which better enables Developers to handle errors.
    
    Added isValidInventorySize(int) method to test if an integer is a valid Bukkit compatible inventory size.
    
    Added 1.7.X and 1.8.X features to the API to enable Coloured Wool for older versions, etc. etc.
    
    Added null check for the ItemMeta within getItem(UIEntry) because it can return Null on broken occasions.
    
    Added isExperimental() which determines if the server is running 1.7.X or 1.8.X server software.
    
    Changed cancelTask slightly as there is no check if the UUID actually has a task which could cancel tasks which could hurt other systems on the server.

--- com/devmclovin/gui/UIManager.java
+++ com/devmclovin/gui/UIManager.java
@@ -2,10 +2,12 @@ package com.devmclovin.gui;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
@@ -49,22 +51,23 @@ public class UIManager {
      *
      * @param player Player object.
      * @param userInterface UserInterface Object.
+     * @param taskDelay Delay of the Update task.
+     * @param taskPeriod Period of the Update task.
+     * @see BukkitRunnable#runTaskTimer(Plugin, long, long)
      */
-    public void openGUI(Player player, UserInterface userInterface) {
+    public void openGUI(Player player, UserInterface userInterface, long taskDelay, long taskPeriod) {
         player.openInventory(setupInventory(userInterface));
-        Bukkit.getPluginManager().registerEvents(new UIListeners(plugin, userInterface, this), plugin);
+        Bukkit.getPluginManager().registerEvents(new UIListeners(userInterface, this), plugin);
         userInterface.onOpen();
-
         BukkitTask task = new BukkitRunnable() {
             @Override
             public void run() {
                 for (UIEntry entry : userInterface.getEntries()) {
                     entry.onUpdate();
-                    userInterface.getInventory().setItem(entry.getSlot(), getItem(entry, userInterface));
+                    userInterface.getInventory().setItem(entry.getSlot(), getItem(entry));
                 }
             }
-        }.runTaskTimer(plugin, 20, 20);
-
+        }.runTaskTimer(plugin, taskDelay, taskPeriod);
         this.tasks.put(player.getUniqueId(), task.getTaskId());
     }
 
@@ -76,32 +79,80 @@ public class UIManager {
      * </p>
      *
      * @param userInterface {@link UserInterface} object (not null).
+     * @throws IllegalStateException if {@link UserInterface} is null.
+     * @throws IllegalArgumentException if the {@link UserInterface#getSize()} is invalid.
      * @return {@link Inventory} instance.
      */
-    private Inventory setupInventory(UserInterface userInterface) {
-        Inventory inventory = Bukkit.createInventory(null, userInterface.getSize(), ChatColor.translateAlternateColorCodes('&', userInterface.getTitle()));
-        userInterface.getEntries().forEach(UIEntry -> inventory.setItem(UIEntry.getSlot(), getItem(UIEntry, userInterface)));
+    private Inventory setupInventory(UserInterface userInterface)
+            throws IllegalStateException, IllegalArgumentException {
+        if (userInterface == null) {
+            throw new IllegalStateException("UserInterface is null.");
+        }
+        if (isValidInventorySize(userInterface.getSize())) {
+            throw new IllegalArgumentException("UserInterface size isn't 9, 18, 27, 36, 45 or 54.");
+        }
+        Inventory inventory = Bukkit.createInventory(
+                null,
+                userInterface.getSize(),
+                ChatColor.translateAlternateColorCodes('&', userInterface.getTitle())
+        );
+        userInterface.getEntries().forEach(UIEntry -> inventory.setItem(UIEntry.getSlot(), getItem(UIEntry)));
         userInterface.setInventory(inventory);
         return userInterface.getInventory();
+    }
+
+    /**
+     * Method to determine if an integer could be a valid Inventory size.
+     * <p>
+     *     Method uses basic math to determine if the Inventory size is valid for the Bukkit API.
+     * </p>
+     *
+     * @param size Integer size to test.
+     * @return {@code true} = is valid, {@code false} = is not valid.
+     */
+    private boolean isValidInventorySize(int size) {
+        return size <= 54 && size >= 0 && size % 9 == 0;
     }
 
     /**
      * Method to <em>get</em> an <em>Item</em>.
      * <p>
      *     This method uses all functionality from {@link UIEntry} to create a {@link Bukkit} {@link ItemStack}.
+     *     <br>This method uses deprecated methods to Experimentally support 1.8.X/1.7.X.
      * </p>
      *
-     * @param UIEntry {@link UIEntry} object.
-     * @param userInterface {@link UserInterface} object.
+     * @param entry {@link UIEntry} object.
      * @return {@link ItemStack} object.
      */
-    private ItemStack getItem(UIEntry UIEntry, UserInterface userInterface) {
-        ItemStack item = new ItemStack(UIEntry.getType(), UIEntry.getQuantity());
+    @SuppressWarnings("deprecation")
+    private ItemStack getItem(UIEntry entry) {
+        ItemStack item;
+        if (isExperimental()) {
+            item = new ItemStack(entry.getType(), entry.getQuantity(), entry.getData());
+        } else {
+            item = new ItemStack(entry.getType(), entry.getQuantity());
+        }
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', UIEntry.getName()));
-        meta.setLore(UIEntry.getLore());
-        item.setItemMeta(meta);
+        if (meta != null) {
+            meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', entry.getName()));
+            meta.setLore(entry.getLore());
+            item.setItemMeta(meta);
+        }
         return item;
+    }
+
+    /**
+     * Method to return if the Server Version is running an Experimentally supported Version.
+     * <p>
+     *     All versions of 1.8.X and 1.7.X will be supported by this API, but not officially.
+     * </p>
+     *
+     * @return {@code true} = 1.7-1.8.X, {@code false} = 1.9.X+
+     */
+    private boolean isExperimental() {
+        String a = Bukkit.getServer().getClass().getPackage().getName();
+        String version = a.substring(a.lastIndexOf('.') + 1);
+        return version.startsWith("v1_8_") || version.startsWith("v1_7_");
     }
 
     /**
@@ -114,7 +165,8 @@ public class UIManager {
      * @param id {@link UUID} of the {@link Player}.
      */
     public void cancelTask(UUID id) {
-        int task = this.tasks.get(id);
+        int task = this.tasks.getOrDefault(id, -1);
+        if (task == -1) return; // If the task isn't found.
         Bukkit.getScheduler().cancelTask(task);
     }
 
